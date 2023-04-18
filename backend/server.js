69197703ed9b0f17ddd3dccf7b858caba928c0d6
Marcus Lucas <marcuslucas@Marcuss-MBP.fios-router.home>
2023-04-18 11:48:12 -0400

    second commit

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,25 @@
-console.log("Server...");
+const express = require("express");
+const dotenv = require("dotenv").config();
+const colors = require("colors");
+const { errorHandler } = require("./middleware/errorMiddleware");
+const connectDB = require("./config/db");
+const PORT = process.env.PORT || 8000;
+
+// connect database
+connectDB();
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.get("/", (req, res) => {
+  res.json({ message: "Welcome to the support desk api" });
+});
+
+// Routes
+app.use("/api/users", require("./routes/userRoutes"));
+
+app.use(errorHandler);
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
